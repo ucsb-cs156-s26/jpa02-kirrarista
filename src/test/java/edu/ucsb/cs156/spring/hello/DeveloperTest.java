13512d33d4d7a39e5b996c9af8f5d84e38e49b50
kirrarista <kirill.aristarkhov@gmail.com>
2026-04-07 22:57:47 -0700

    ka - updated with actual github id

--- src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
+++ src/test/java/edu/ucsb/cs156/spring/hello/DeveloperTest.java
@@ -25,7 +25,9 @@ public class DeveloperTest {
         assertEquals("Kirill", Developer.getName());
     }
 
-    // TODO: Add additional tests as needed to get to 100% jacoco line coverage, and
-    // 100% mutation coverage (all mutants timed out or killed)
+    @Test
+    public void getGithubId_returns_correct_githubId() {
+        assertEquals("kirrarista", Developer.getGithubId());
+    }
 
 }
